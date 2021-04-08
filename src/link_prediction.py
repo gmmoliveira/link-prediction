d9177b461cc972d2f69bed0fb6cdad45a80564a5
gmmoliveira <gmmoliveira1@gmail.com>
2021-04-08 15:25:37 -0300

    added class equilibrium for traning, employed a xgboost model which runs on the GPU (a previous project I have written), employed a deep learning transformer model as well. Added computation metrics which are suitable for imbalanced datasets.

--- src/link_prediction.py
+++ src/link_prediction.py
@@ -1,15 +1,39 @@
+'''
+Copyright 2021 Guilherme Oliveira
+SPDX-License-Identifier: Apache-2.0
+========================================================================================================================
+Author: Guilherme Oliveira
+Date: april 08, 2021
+Contact: gmmoliveira1@gmail.com
+License: Apache-2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+========================================================================================================================
+This script implements functions presenting how to perform link prediction on undirected unweighted/weighted graphs,
+using unsupervised learning to obtain vertices embeddings, where the graph may have an extremely
+imbalance between edges and non-edges.
+
+A non-edge may be defined as: given a graph G(V, E), further, given
+a pair of vertices v1 and v2 which belongs to V, a non-edge is a non-existing link between v1 and v2, that is,
+the edge (v1, v2) doesn't belong to E.
+========================================================================================================================
+'''
+
 import numpy as np
-import pandas as pd
 import networkx as nx
-import numba as nb
 from nbne import train_model
-from collections import defaultdict
 from os.path import join, abspath, dirname
 from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score, recall_score
 from time import time
 import xgboost
+import tensorflow as tf
+import os
+import warnings
+import xgboost_gpu
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.get_logger().setLevel('INFO')
 
 
 def get_nbne_embeddings(
@@ -79,10 +103,10 @@ def _read_graph(file_name, adj_matrix_dtype=np.float64):
 	return G, A
 
 
-@nb.njit(fastmath=True)
-def build_link_prediction_train(A, emb, sampling_rate=0.5, dtype=np.float64):
+def assemble_balanced_link_prediction_data(A, emb, sampling_rate=0.5, dtype=np.float32):
 	'''
-	Generates training/testing data for graph "A" using it's embeddings "emb". This function is meant to be used with
+	Generates class balanced training/testing data for graph "A" using it's embeddings "emb". This function
+	is meant to be used with
 	undirected unweighted/weighted graphs, since it only considers the upper diagonal of "A" due to efficiency purposes.
 	Self loops are also ignored;
 
@@ -95,74 +119,175 @@ def build_link_prediction_train(A, emb, sampling_rate=0.5, dtype=np.float64):
 	:return:
 		* X_train: a 2D numpy.ndarray representing the training features;
 		* y_train: a 1D numpy.ndarray representing the training labels/targets;
-	'''
-	num_nodes = A.shape[0]
-	emb_dimms = emb.shape[1]
-	expected_instances = int(sampling_rate * (((num_nodes ** 2) - num_nodes) / 2))
-	if expected_instances <= 0:
-		expected_instances = 1
-	elif expected_instances > ((num_nodes ** 2) - num_nodes):
-		expected_instances = (num_nodes ** 2) - num_nodes
+		'''
+	n = A.shape[0]
+	max_instances = int((((n ** 2) - n) / 2) * sampling_rate)
+	zeros_idx, ones_idx = [], []
+	for i in range(n):
+		for j in range(i + 1, n - 1):
+			if float(A[i, j]) == 0.0:
+				zeros_idx.append((i, j, 0))
+			else:
+				ones_idx.append((i, j, 1))
+	dominant_class = 1 if len(ones_idx) > len(zeros_idx) else 0
+	np.random.shuffle(zeros_idx)
+	np.random.shuffle(ones_idx)
+	if dominant_class == 0:
+		zeros_idx = zeros_idx[:len(ones_idx)]
+	else:
+		ones_idx = ones_idx[:len(zeros_idx)]
+	if len(zeros_idx) + len(ones_idx) > max_instances:
+		diff = np.ceil(np.abs(len(zeros_idx) + len(ones_idx) - max_instances) / 2)
+		zeros_idx = zeros_idx[:-diff]
+		ones_idx = ones_idx[:-diff]
 
-	selected_nodes = np.empty(shape=(int(((num_nodes ** 2) - num_nodes) / 2), 2), dtype=np.int64)
-	k = 0
-	for i in range(num_nodes - 1):
-		for j in range(i + 1, num_nodes):
-			selected_nodes[k, 0] = i
-			selected_nodes[k, 1] = j
-			k += 1
-	np.random.shuffle(selected_nodes)
-	selected_nodes = selected_nodes[:expected_instances]
+	data = np.empty(shape=(len(zeros_idx) + len(ones_idx), 2 * emb.shape[1] + 1), dtype=dtype)
+	for k, (i, j, label) in enumerate(zeros_idx + ones_idx):
+		data[k, :emb.shape[1]] = emb[i, :]
+		data[k, emb.shape[1]:-1] = emb[j, :]
+		data[k, -1] = label
+	np.random.shuffle(data)
+	x = data[:, :-1]
+	y = data[:, -1]
 
-	x = np.empty(shape=(expected_instances, 2 * emb_dimms), dtype=dtype)
-	y = np.empty(shape=expected_instances, dtype=dtype)
-	for k in range(expected_instances):
-		i = selected_nodes[k, 0]
-		x[k, :emb_dimms] = emb[i]
-		j = selected_nodes[k, 1]
-		x[k, emb_dimms:] = emb[j]
-		y[k] = A[i, j]
 	return x, y
+
+
+def print_scores_summary(ytrue, ypredicted, training_time, inference_time, model_name="Model"):
+	def fmt_fixed_len(str, fixed_length=25):
+		assert fixed_length >= len(str)
+		n = fixed_length - len(str)
+		return str + (" " * n)
+
+	print("{} {} results {}".format("=" * 5, model_name, "=" * 5))
+	print("{}{:.2f}%".format(fmt_fixed_len("Accuracy:"), accuracy_score(ytrue, ypredicted) * 100))
+	print("{}{:.2f}%".format(fmt_fixed_len("ROC AUC:"), roc_auc_score(ytrue, ypredicted) * 100))
+	print("{}{:.2f}%".format(fmt_fixed_len("F1:"), f1_score(ytrue, ypredicted) * 100))
+	print("{}{:.2f}%".format(fmt_fixed_len("Precision:"), precision_score(ytrue, ypredicted) * 100))
+	print("{}{:.2f}%".format(fmt_fixed_len("Recall:"), recall_score(ytrue, ypredicted) * 100))
+	print("{} training time: {:.2f} seconds.".format(model_name, training_time))
+	print("{} inference time: {:.2f} seconds.".format(model_name, inference_time))
+
+
+def make_nn(input_size):
+	model_inputs = tf.keras.Input(shape=input_size, dtype=tf.float32)
+	#layer = tf.keras.layers.Reshape((2, -1))(model_inputs)
+	half = int(input_size / 2)
+	layer0 = tf.keras.layers.Reshape((1, -1))(model_inputs[:, :half])
+	layer1 = tf.keras.layers.Reshape((1, -1))(model_inputs[:, half:])
+
+	a0 = tf.keras.layers.MultiHeadAttention(num_heads=16, key_dim=16, value_dim=16, dropout=0.0)(layer0, layer0)
+	a00 = tf.keras.layers.LayerNormalization()(a0 + layer0)
+	a0 = tf.keras.layers.Concatenate(axis=1)([a0, a00])
+	a1 = tf.keras.layers.MultiHeadAttention(num_heads=16, key_dim=16, value_dim=16, dropout=0.0)(layer1, layer1)
+	a11 = tf.keras.layers.LayerNormalization()(a1 + layer1)
+	a1 = tf.keras.layers.Concatenate(axis=1)([a1, a11])
+	a2 = tf.keras.layers.MultiHeadAttention(num_heads=16, key_dim=16, value_dim=16, dropout=0.0)(layer0, layer1)
+	a22 = tf.keras.layers.LayerNormalization()(a2 + layer0 + layer1)
+	a2 = tf.keras.layers.Concatenate(axis=1)([a2, a22])
+	a3 = tf.keras.layers.MultiHeadAttention(num_heads=16, key_dim=16, value_dim=16, dropout=0.0)(layer1, layer0)
+	a33 = tf.keras.layers.LayerNormalization()(a3 + layer1 + layer0)
+	a3 = tf.keras.layers.Concatenate(axis=1)([a3, a33])
+	layer = tf.keras.layers.Concatenate(axis=1)([a0, a1, a2, a3])
+
+	partially_encoded = tf.keras.layers.Flatten()(layer)
+
+	layer = tf.keras.layers.Dense(128, activation="relu")(partially_encoded)
+	layer = tf.keras.layers.Dense(partially_encoded.shape[-1], activation="relu")(layer)
+	layer = tf.keras.layers.LayerNormalization()(layer + partially_encoded)
+	layer = tf.keras.layers.Concatenate(axis=-1)([layer, partially_encoded])
+	model_outputs = tf.keras.layers.Dense(128, activation="sigmoid")(layer)
+
+	model_outputs = tf.keras.layers.Dense(1, activation="sigmoid")(layer)
+
+	model = tf.keras.Model(inputs=model_inputs, outputs=model_outputs)
+	model.compile(optimizer="adam",
+	              loss="binary_crossentropy",
+	              metrics=["binary_accuracy", "AUC", tf.keras.metrics.FalsePositives(), tf.keras.metrics.TruePositives()]
+	              )
+	return model
 
 
 if __name__ == "__main__":
 	start_time = time()
-	"""
-	This invoking of the "build_link_prediction_train(...)" is meant only to force numba to compile the function to C
-	without including the meaningful compiling time in the final printed time. This method of compiling ahead of time
-	is not recommended, it's just a quick and dirty version, for the right way to do so, check numba's documentation:
-	"https://numba.pydata.org/numba-doc/dev/user/pycc.html".
-	"""
-	build_link_prediction_train(np.empty(shape=(2, 2)), np.empty(shape=(2, 2)))
-	print("Numba's compilation time of the \"build_link_prediction_train(...)\" function: {:.2f} seconds"
-				.format(time() - start_time))
-	start_time = time()
-	"""
-	graph retrieved from the page "https://snap.stanford.edu/data/egonets-Facebook.html", by downloading the file
-	"facebook_combined.txt.gz". The full download link is "https://snap.stanford.edu/data/facebook_combined.txt.gz".
-	"""
-	graph_file_name = "facebook_combined.txt"
-	G, A = _read_graph(graph_file_name)
-	emb = get_nbne_embeddings(
-								G,
-								num_permutations=10,
-								embedding_dimension=8,
-								window_size=5,
-								min_count=0,
-								min_degree=0,
-								workers=8
-								)
-	# normalizing the embeddings should, generally, be a good a idea
-	emb = normalize(emb)
-	# here, one might use sampling_rate=1.0 to combine the links and non-links between all vertices, then the data is
-	# split among training and testing by the sklearn.train_test_split
-	x, y = build_link_prediction_train(A, emb, sampling_rate=0.10, dtype=np.float64)
-	xtrain, xtest, ytrain, ytrue = train_test_split(x, y, test_size=1/2, shuffle=True)
-	xgb_model = xgboost.XGBClassifier()
-	xgb_model.fit(xtrain, ytrain)
-	ypredicted = xgb_model.predict(xtest)
+	EMB_DIM_COUNT = 256
+	BATCH_SIZE = 128
+	with warnings.catch_warnings():
+		warnings.simplefilter("ignore")
+		"""
+		graph retrieved from the page "https://snap.stanford.edu/data/egonets-Facebook.html", by downloading the file
+		"facebook_combined.txt.gz". The full download link is "https://snap.stanford.edu/data/facebook_combined.txt.gz".
+		"""
+		read_graph_tstart = time()
+		graph_file_name = "facebook_combined.txt"
+		G, A = _read_graph(graph_file_name)
+		print("Graph read in: {:.2f} seconds".format(time() - read_graph_tstart))
+		emb_tstart = time()
+		emb = get_nbne_embeddings(
+									G,
+									num_permutations=10,
+									embedding_dimension=EMB_DIM_COUNT,
+									window_size=5,
+									min_count=0,
+									min_degree=3,
+									workers=8
+									)
+		# normalizing the embeddings should, generally, be a good a idea
+		emb = normalize(emb)
+		print("Vertices embeddings trained in: {:.2f} seconds".format(time() - emb_tstart))
+		# here, one might use sampling_rate=1.0 to combine the links and non-links between all vertices, then the data is
+		# split among training and testing by the sklearn.train_test_split
+		assemble_tstart = time()
+		x, y = assemble_balanced_link_prediction_data(A, emb, sampling_rate=0.10, dtype=np.float64)
+		xtrain, xtest, ytrain, ytrue = train_test_split(x, y, test_size=1/4, shuffle=True)
+		print("Training and testing data assembled in: {:.2f} seconds".format(time() - assemble_tstart))
 
-	print("Accuracy: {:.2f}%".format(accuracy_score(ytrue, ypredicted) * 100))
-	print("ROC AUC: {:.2f}%".format(roc_auc_score(ytrue, ypredicted) * 100))
-	print("Total elapsed time without considering numba's compilation: {:.2f} seconds.".format(time() - start_time))
+		xgb_train_tstart = time()
+		xgb_dict = xgboost_gpu.train_xgboost_gpu(
+										xtrain,
+										ytrain,
+										data_chunksize=None,
+										n_gpus=2,
+										n_threads_per_gpu=10,
+										params={"objective": "binary:logistic", "eval_metric": "logloss"}
+										)
+		xgb_model = xgb_dict['booster']
+		xgb_train_telapsed = time() - xgb_train_tstart
+		xgb_inference_tstart = time()
+
+		ypredicted = xgboost_gpu.predict_xgboost_gpu(
+										xgb_model,
+										xtest,
+										data_chunksize=None,
+										n_gpus=1,
+										n_threads_per_gpu=20,
+										)
+		ypredicted = np.round(ypredicted)
+		xgb_inference_telapsed = time() - xgb_inference_tstart
+		print_scores_summary(ytrue, ypredicted, xgb_train_telapsed, xgb_inference_telapsed, model_name="XGBoost (GPU)")
+
+		xgb_inference_tstart = time()
+		ypredicted = xgb_model.predict(xgboost.DMatrix(xtest, feature_names=[str(k) for k in range(xtest.shape[1])]))
+		ypredicted = np.round(ypredicted)
+		print("XGBoost (CPU) inference time: {:.2f} seconds.".format(time() - xgb_inference_tstart))
+
+		# =============================================================================
+		model = make_nn(EMB_DIM_COUNT * 2)
+		print(model.summary())
+
+		def scheduler(epoch, lr):
+			return lr * tf.math.exp(-0.06)
+		lr_scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+		transformer_train_tstart = time()
+		model.fit(xtrain, ytrain, epochs=50, batch_size=BATCH_SIZE, callbacks=[lr_scheduler])
+		cnn_train_telapsed = time() - transformer_train_tstart
+
+		cnn_inference_tstart = time()
+		ypredicted = model.predict(xtest)
+		ypredicted = np.round(ypredicted)
+		cnn_iference_telapsed = time() - cnn_inference_tstart
+		print_scores_summary(ytrue, ypredicted, cnn_train_telapsed, cnn_iference_telapsed, model_name="Transformer")
+
+		print("Total script execution time: {:.2f} seconds.".format(time() - start_time))
 
---
+++ src/xgboost_gpu.py
@@ -0,0 +1,248 @@
+'''
+Copyright 2021 Guilherme Oliveira
+SPDX-License-Identifier: Apache-2.0
+========================================================================================================================
+Author: Guilherme Oliveira
+Date: july 06, 2020
+Contact: gmmoliveira1@gmail.com
+License: Apache-2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+========================================================================================================================
+This script implements functions to facilitate the execution of the XGBoost algorithm on multiple GPUs on a single-
+machine.
+========================================================================================================================
+'''
+from xgboost.dask import DaskDMatrix, train as dask_xgboost_train, predict as dask_xgboost_predict
+from dask.dataframe import from_array, from_pandas
+from dask.distributed import Client
+from dask_cuda import LocalCUDACluster
+
+import numpy as np
+import pandas as pd
+
+
+def train_xgboost_gpu(
+		X, y,
+		data_chunksize=None,
+		n_gpus=None, n_threads_per_gpu=1,
+		params=None,
+		xgboost_model=None,
+		gpu_cluster=None, client=None
+):
+	'''
+	Trains a XGBoost model on the GPU.
+
+	:param X: a 2D matrix object of either type numpy ndarray or pandas DataFrame;
+	:param y: a 1D array of one of the following types: numpy ndarray, pandas Series or pandas DataFrame;
+
+	:param data_chunksize: number of rows to partition input data (both X and y simultaneously) to split among multiple
+		GPU devices. Default value None splits evenly among devices;
+	:param n_gpus: number of GPUs to be used. Default value None selects all available devices.
+	:param n_threads_per_gpu: number of threads per GPU;
+	:param params: xgboost trainning params as a python dict, refer to
+		https://xgboost.readthedocs.io/en/latest/parameter.html
+	:param xgboost_model: xgbooster object to continue training, it may be either a regular XGBoost model or a
+		dask xgboost dict
+	:param gpu_cluster: an existing dask cluster object to use. This param should be used if you call this method
+		too many times in quick successions. Note that this function doesn't close an externally created cluster.
+	:param client: an existing dask client object to use. This param should be used if you call this method
+		too many times in quick successions. Note that this function doesn't close an externally created client.
+
+	:return:
+	A dictionary containing 2 keys:
+		* 'booster': maps to a XGBoost model
+		* 'history': maps to  another dict which informs the history of the training process, as in the following the
+			examṕle: {'train': {'logloss': ['0.48253', '0.35953']}, 'eval': {'logloss': ['0.480385', '0.357756']}}}
+	'''
+
+	if gpu_cluster is None:
+		local_gpus = LocalCUDACluster(n_workers=n_gpus, threads_per_worker=n_threads_per_gpu)
+	else:
+		local_gpus = gpu_cluster
+	if client is None:
+		local_dask_client = Client(local_gpus, {'verbose': 0})
+	else:
+		local_dask_client = client
+
+	if data_chunksize is None:
+		data_chunksize = X.shape[0] // len(local_gpus.cuda_visible_devices)
+	if params is None:
+		params = {
+			'learning_rate': 0.3,
+			'max_depth': 8,
+			'objective': 'reg:squarederror',
+			'verbosity': 0,
+			'tree_method': 'gpu_hist'
+		}
+
+	if isinstance(X, pd.DataFrame):
+		X = from_pandas(X, chunksize=data_chunksize)
+	else:
+		X = from_array(X, chunksize=data_chunksize)
+	if isinstance(y, pd.DataFrame):
+		y = from_pandas(y, chunksize=data_chunksize)
+	else:
+		y = from_array(y, chunksize=data_chunksize)
+	dtrain = DaskDMatrix(local_dask_client, X, y)
+
+	if type(xgboost_model) is dict:
+		xgboost_model = xgboost_model['booster']
+
+	xgb_model = dask_xgboost_train(local_dask_client, params, dtrain, num_boost_round=100, evals=[(dtrain, 'train')],
+	                               xgb_model=xgboost_model)
+
+	if client is None:
+		local_dask_client.close()
+	if gpu_cluster is None:
+		local_gpus.close()
+
+	return xgb_model
+
+
+def predict_xgboost_gpu(
+		xgb_model, X,
+		data_chunksize=None,
+		n_gpus=None, n_threads_per_gpu=1,
+		gpu_cluster=None, client=None
+):
+	'''
+	Predicts the output for the input features X using the 'xgb_model' running on the GPU.
+
+	:param xgb_model: a dask XGBoost model to use for predictions
+	:param X: the input features to use for predictions, must be either a numpy ndarray or a pandas DataFrame
+	:param data_chunksize: chunk sizes to be used on a dask dataframe, leave the default value None for auto decision
+	:param n_gpus: number of GPUs to be used. Default value None selects all available devices;
+	:param n_threads_per_gpu: number of threads per GPU;
+	:param gpu_cluster: an existing dask cluster object to use. This param should be used if you call this method
+		too many times in quick successions. Note that this function doesn't close an externally created cluster.
+	:param client: an existing dask cluster object to use. This param should be used if you call this method
+		too many times in quick successions. Note that this function doesn't close an externally created client.
+	:return:
+		If the input features X is a pandas DataFrame, returns a array-like DataFrame of single column containing
+		the predictions;
+
+		Otherwise, if the input features X is a numpy ndarray, returns a 1D ndarray containing the predictions .
+	'''
+	if gpu_cluster is None:
+		local_gpus = LocalCUDACluster(n_workers=n_gpus, threads_per_worker=n_threads_per_gpu)
+	else:
+		local_gpus = gpu_cluster
+	if client is None:
+		local_dask_client = Client(local_gpus)
+	else:
+		local_dask_client = client
+
+	if data_chunksize is None:
+		data_chunksize = X.shape[0] // len(local_gpus.cuda_visible_devices)
+
+	if isinstance(X, pd.DataFrame):
+		ndarray = False
+		X = from_pandas(X, chunksize=data_chunksize)
+	else:
+		ndarray = True
+		X = from_array(X, chunksize=data_chunksize)
+
+	y_predicted = dask_xgboost_predict(local_dask_client, xgb_model, X)
+	y_predicted = pd.DataFrame(y_predicted)
+
+	if client is None:
+		local_dask_client.close()
+	if gpu_cluster is None:
+		local_gpus.close()
+
+	if ndarray:
+		return y_predicted.to_numpy()
+	return y_predicted
+
+
+def _example():
+	# the following imports are meant to be used only in the scope of this example, therefore,
+	# they were placed here for performance issues regarding external modules calling this one
+	from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
+	from sklearn.metrics import explained_variance_score, mean_squared_error, max_error
+	from os.path import exists
+
+	base_path = ''
+	if exists('../models/'):
+		base_path = '../models/'
+
+	# [WARNING]: choose carefully the below parameters according to your machine, avoiding, for example, consuming
+	# more memory than what's available
+	n, m = 10 ** 4, 10
+	rand = np.random.Generator(np.random.PCG64())
+
+	print('========== *** XGBoost Classification example *** ==========')
+	params = {
+		'learning_rate': 0.3,
+		'max_depth': 8,
+		'objective': 'binary:hinge',
+		'verbosity': 0,
+		'tree_method': 'gpu_hist'
+	}
+	class_proportion = 0.5
+	X = rand.random(size=(n, m))
+	y = np.array([1 if np.sum(X[i, :]) > class_proportion * m else 0 for i in range(X.shape[0])])
+	classification_xgbmodel = train_xgboost_gpu(X, y, params=params, n_gpus=1, n_threads_per_gpu=1, xgboost_model=None)
+
+	X = rand.random(size=(n, m))
+	y = np.array([1 if np.sum(X[i, :]) > class_proportion * m else 0 for i in range(X.shape[0])])
+	y_pred = predict_xgboost_gpu(classification_xgbmodel, X, n_gpus=1, n_threads_per_gpu=1)
+	'''
+	# my tests have shown that predicting over the GPU is much slower than over the CPU
+	# to predict using the CPU instead of the GPU, use the following example code
+	from xgboost import DMatrix
+	y_pred = classification_xgbmodel['booster'].predict(DMatrix(pd.DataFrame(X, columns=[i for i in range(m)])))
+	'''
+
+	acc = accuracy_score(y, y_pred)
+	cm = confusion_matrix(y, y_pred)
+	print('accuracy: {:.2f}%'.format(acc * 100))
+	print('confusion matrix:')
+	print(cm)
+	try:
+		print('ROC AUC score: {:.2f}%'.format(roc_auc_score(y, y_pred) * 100))
+	except:
+		pass
+
+	# save your model as follows
+	classification_xgbmodel['booster'].save_model(base_path + 'my_classf_model001.xgbmodel')
+
+	print('========== *** XGBoost Regression example *** ==========')
+	transformation = rand.random(size=m)
+	X = rand.random(size=(n, m))
+	y = np.matmul(X, transformation)
+	params = {
+		'learning_rate': 0.3,
+		'max_depth': 8,
+		'objective': 'reg:squarederror',
+		'verbosity': 0,
+		'tree_method': 'gpu_hist'
+	}
+	regression_xgbmodel = train_xgboost_gpu(X, y, params=params)
+	X = rand.random(size=(n, m))
+	y = np.matmul(X, transformation)
+	y_pred = predict_xgboost_gpu(regression_xgbmodel, X)
+	'''
+	# my tests have shown that predicting over the GPU is much slower than over the CPU
+	# to predict using the CPU instead of the GPU, use the following example code
+	from xgboost import DMatrix
+	y_pred = regression_xgbmodel['booster'].predict(DMatrix(pd.DataFrame(X, columns=[i for i in range(m)])))
+	'''
+
+	vscore = explained_variance_score(y, y_pred)
+	mse = mean_squared_error(y, y_pred)
+	me = max_error(y, y_pred)
+	print('Variance score: {:.2f}'.format(vscore))
+	print('Mean squared error: {:.2f}'.format(mse))
+	print('Maximum absolute error: {:.2f}'.format(me))
+
+	# save your model as follows
+	regression_xgbmodel['booster'].save_model(base_path + 'my_reg_model001.xgbmodel')
+
+
+if __name__ == '__main__':
+	from time import time
+
+	t_start = time()
+	_example()
+	t_end = time() - t_start
+	print('executed in {:.2f} seconds'.format(t_end))
